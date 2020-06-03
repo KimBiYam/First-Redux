4d2c941dccce9ec8663b55847128489f317fbe40
hot9998 <hot9998@naver.com>
2020-06-04 04:54:18 +0900

    update

---
+++ src/components/AddNumber.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class AddNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input type="button" value="+"></input>
+        <input type="text" value="0"></input>
+      </div>
+    );
+  }
+}
