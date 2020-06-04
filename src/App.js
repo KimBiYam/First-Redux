6635f01172d3a36ea7bf81f5bc52f282b7ad8b37
hot9998 <hot9998@naver.com>
2020-06-04 17:40:46 +0900

    마무리

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 import AddNumberRoot from "./components/AddNumberRoot";
 import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Root</h1>
-      <AddNumberRoot></AddNumberRoot>
-      <DisplayNumberRoot></DisplayNumberRoot>
-    </div>
-  );
+class App extends Component {
+  state = { number: 0 };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
